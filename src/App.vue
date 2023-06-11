18deff0a06b8b97d9593445e5006b388631ba753
RattusInVitaReali <46037351+RattusInVitaReali@users.noreply.github.com>
2023-06-11 17:20:52 +0200

    unugly

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,13 @@
 <template>
+  <link rel="preconnect" href="https://fonts.googleapis.com" />
+  <link rel="preconnect" href="https://fonts.gstatic.com" />
+  <link
+    href="https://fonts.googleapis.com/css2?family=Raleway:wght@700&display=swap"
+    rel="stylesheet"
+  />
   <div id="body">
-    <nav class="navbar navbar-expand-lg" id="navbar">
+    <nav class="navbar navbar-expand-lg padded">
       <div class="container-fluid">
-        <a class="navbar-brand" href="#">Navbar</a>
         <button
           class="navbar-toggler"
           type="button"
@@ -15,13 +20,19 @@
           <span class="navbar-toggler-icon"></span>
         </button>
         <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
-          <div class="navbar-nav">
-            <router-link class="nav-link" to="/">Home</router-link>
-            <router-link class="nav-link" to="/about">About</router-link>
+          <div class="navbar-nav mx-auto justify-content-between w-100">
+            <div></div>
+            <router-link class="nav-link" to="/">HOME</router-link>
+            <router-link class="nav-link" to="/about">ABOUT</router-link>
+            <router-link class="nav-link" to="/artworks">ARTWORKS</router-link>
+            <router-link class="nav-link" to="/artists">ARTISTS</router-link>
+            <router-link class="nav-link" to="/">MY PROFILE</router-link>
+            <div></div>
           </div>
         </div>
       </div>
     </nav>
+    <div class="text-center big-thing">{{ getBigThing() }}/</div>
     <div class="row">
       <div
         class="rounded-2 py-3 col-md-8 col-12 offset-md-2 h-auto"
@@ -33,24 +44,60 @@
   </div>
 </template>
 
+<script>
+import Data from "@/assets/data.js";
+
+export default {
+  data() {
+    return {
+      artworks: Data.artworks,
+      artists: Data.artists,
+    };
+  },
+  methods: {
+    getBigThing() {
+      let bt = this.$route.path.toUpperCase();
+      console.log(this.$route.path);
+      if (bt == "/") bt = "/SIMPLICITY";
+      switch (bt) {
+        case "/":
+          bt = "/SIMPLICITY";
+          break;
+        case "/ARTWORK":
+          let artworkName = this.artworks
+            .find((awork) => (awork.id = this.$route.query.id))
+            .name.toUpperCase();
+          bt = "/" + artworkName;
+          break;
+      }
+      return bt;
+    },
+  },
+};
+</script>
+
 <style lang="less">
-@navbar-color: #be6eff;
+@navbar-color: #dbafff;
 @text-color: #474747;
-@background-color: #fffca8;
-@background-content: #f7f7f7;
+@background-color: #ffffff;
+@background-content: #e9e9e9;
 #navbar {
   background: @navbar-color;
+  padding: 60px !important;
+}
+.padded {
+  padding: 68px !important;
 }
 #body {
-  height: calc(50vh);
+  height: 510px;
   background-color: @navbar-color;
+  font-family: "Raleway", sans-serif;
 }
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: "Raleway", sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
 }
 #main-view {
   background: @background-content;
@@ -58,16 +105,14 @@
   margin-bottom: 25px;
   padding: 15px;
 }
-nav {
-  padding: 30px;
+
+.nav-link {
+  font-size: x-large !important;
 }
 
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-nav a.router-link-exact-active {
-  color: @text-color;
+.big-thing {
+  padding-bottom: 75px;
+  font-size: 70px !important;
+  color: black;
 }
 </style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,30 +1,11 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png" />
-    <div class="col-12">
-      <div class="card">
-        <img src="../assets/logo.png" class="card-img-top" alt="..." />
-        <div class="card-body">
-          <h5 class="card-title">Card title</h5>
-          <p class="card-text">
-            Some quick example text to build on the card title and make up the
-            bulk of the card's content.
-          </p>
-          <a href="#" class="read-more float-right">Read more -></a>
-        </div>
-      </div>
-    </div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
-
 export default {
   name: "HomeView",
-  components: {
-    HelloWorld,
-  },
 };
 </script>
